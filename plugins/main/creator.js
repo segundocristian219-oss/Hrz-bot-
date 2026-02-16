2bb3af43cce38d089b3a08cfc6c8b5e16c5594ef
deylin-16 <deylin1616@gmail.com>
2026-02-16 11:53:00 -0600

    Actualizar creator.js

--- plugins/main/creator.js
+++ plugins/main/creator.js
@@ -3,27 +3,32 @@ const ownerCommand = {
     alias: ['creador', 'contacto', 'admin'],
     category: 'main',
     run: async (m, { conn }) => {
-        const myNumber = '50432955554'
-        const myJid = `${myNumber}@s.whatsapp.net`
-        const pushName = m.pushName || conn.user.name || 'Deylin'
+        const myNumber = '50433191934'
+        const myJid = myNumber + '@s.whatsapp.net'
+        const name = await conn.getName(myJid) || 'Deylin'
 
         const vcard = 'BEGIN:VCARD\n' +
                       'VERSION:3.0\n' +
-                      `FN:${pushName}\n` +
+                      `FN:${name}\n` +
+                      `N:;${name};;;\n` +
+                      'ORG:Deylin - System Admin;\n' +
+                      'TITLE:CEO & Fundador;\n' +
                       `TEL;type=CELL;type=VOICE;waid=${myNumber}:${myNumber}\n` +
                       'X-WA-BIZ-DESCRIPTION:Desarrollador oficial de sistemas de automatización.\n' +
-                      `X-WA-BIZ-NAME:${pushName}\n` +
+                      `X-WA-BIZ-NAME:${name}\n` +
                       'END:VCARD'
 
         await conn.sendMessage(m.chat, {
             contacts: {
-                displayName: pushName,
+                displayName: name,
                 contacts: [{ vcard }]
             },
             contextInfo: {
+                isForwarded: true,
+                forwardingScore: 999,
                 externalAdReply: {
-                    title: `${pushName} ✅`,
-                    body: 'Official Business Account',
+                  //  title: `${name} ✅`,
+                   // body: 'Official Business Account',
                     thumbnailUrl: 'https://ik.imagekit.io/pm10ywrf6f/bot_by_deylin/1771123381140_9u4BT8HVp.jpeg',
                     sourceUrl: `https://wa.me/${myNumber}`,
                     mediaType: 1,
@@ -41,14 +46,14 @@ const ownerCommand = {
                 },
                 message: {
                     contactMessage: {
-                        displayName: pushName,
+                        displayName: name,
                         vcard: vcard
                     }
                 }
             } 
         })
         
-        await m.react('✅')
+        await m.react('🛡️')
     }
 }
 
