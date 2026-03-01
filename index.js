3c8d6b8af185ca042930179129fc170b8efd1b1a
deylin-16 <deylin1616@gmail.com>
2026-02-28 23:09:47 -0600

    Actualizar index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ process.on('uncaughtException', async (err) => {
     try { await uploadCriticalError(err, 'Uncaught Exception Global'); } catch {}
 });
 
-mongoose.connect('mongodb+srv://voker:voker@cluster0.dsle1da.mongodb.net/?appName=Cluster0')
+mongoose.connect('mongodb+srv://voker:voker@cluster0.dsle1da.mongodb.net/catbot?retryWrites=true&w=majority')
     .then(() => console.log(chalk.greenBright('┃ DATABASE: Local MongoDB Conectado')))
     .catch(() => console.log(chalk.red('┃ DATABASE: Error (Asegúrate que MongoDB esté corriendo)')));
 
