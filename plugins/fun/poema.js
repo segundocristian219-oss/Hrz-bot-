9c35aab2a8a3df2d5a6f0125c01ce53272e79ccc
deylin-16 <deylin1616@gmail.com>
2026-03-03 13:58:01 -0600

    Crear poema.js

---
+++ plugins/fun/poema.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+
+const poemaCommand = {
+    name: 'poema',
+    alias: ['verso', 'poesia'],
+    category: 'cultura',
+    run: async (m, { conn, text }) => {
+        const urlRaw = 'https://raw.githubusercontent.com/deylin-16/database/main/src/poemas.json';
+        
+        try {
+            const response = await axios.get(urlRaw);
+            let lista = response.data.poemas_masivos;
+
+            if (text) {
+                lista = lista.filter(p => 
+                    p.autor.toLowerCase().includes(text.toLowerCase()) || 
+                    p.titulo.toLowerCase().includes(text.toLowerCase())
+                );
+            }
+
+            if (lista.length === 0) return;
+
+            const p = lista[Math.floor(Math.random() * lista.length)];
+
+            const mensaje = `📜 *${p.titulo.toUpperCase()}*\n\n${p.texto}\n\n_— ${p.autor}_ (${p.estilo})`;
+
+            await conn.sendMessage(m.chat, { 
+                text: mensaje,
+                mentions: [m.sender] 
+            }, { quoted: m });
+
+        } catch (error) {
+            console.error(error);
+        }
+    }
+};
+
+export default poemaCommand;
