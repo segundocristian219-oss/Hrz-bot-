59c036f33c64709b222143577d8aec231d58df93
deylin-16 <deylin1616@gmail.com>
2026-03-06 15:33:48 -0600

    Actualizar chatgpt.js

--- plugins/ia/chatgpt.js
+++ plugins/ia/chatgpt.js
@@ -6,19 +6,25 @@ const chatgptCommand = {
     category: 'ai',
     run: async (m, { conn, text }) => {
         if (!text) return conn.sendMessage(m.chat, { text: '¡Hola! ¿En qué puedo ayudarte hoy?' }, { quoted: m });
-        
+
         await m.react('⏳');
         try {
             const url = `${global.url_api}/chat?q=${encodeURIComponent(text)}&apikey=${apikey}`;
             const res = await fetch(url);
-            const data = await res.json();
+            const json = await res.json();
 
-            if (!data.response) throw new Error();
+            
+            if (!json.success || !json.data || !json.data.content) {
+                throw new Error("Estructura de API inválida");
+            }
 
             await m.react('✅');
-            await conn.sendMessage(m.chat, { text: data.content }, { quoted: m });
+            
+            
+            await conn.sendMessage(m.chat, { text: json.data.content }, { quoted: m });
 
         } catch (e) {
+            console.error(e);
             await m.react('❌');
             await conn.sendMessage(m.chat, { text: '⚠️ Error al conectar con ChatGPT.' }, { quoted: m });
         }
