a722faf858460ea28c14f470c86cd48057d54a0c
deylin-16 <deylin1616@gmail.com>
2026-03-06 15:41:55 -0600

    Actualizar tiktok.js

--- plugins/download/tiktok.js
+++ plugins/download/tiktok.js
@@ -11,35 +11,32 @@ const tiktok = {
             await m.react("⏳")
 
             const res = await fetch(`${url_api}/tiktok?url=${encodeURIComponent(args[0])}&apikey=${key}`)
-            const data = await res.json()
+            const json = await res.json()
 
-            if (!data.success) throw new Error("API_ERROR")
+            if (!json.success || !json.data) throw new Error("API_ERROR")
 
-            
-            const { 
-                title, play, duration, music_info, author,
-                play_count, digg_count, comment_count, share_count 
-            } = data
+            const data = json.data
+            const videoUrl = data.reproducir || data.play || data.wmplay
             
             const formatter = new Intl.NumberFormat('es-ES')
-            
+
             const caption = `\t\t\t*𝗧𝗜𝗞-𝗧𝗢𝗞 𝗗𝗘𝗦𝗖𝗔𝗥𝗚𝗔𝗦*
 
-> ღ *Autor:* ${author?.nickname || 'Anónimo'}
-> ✎ *Título:* ${title || 'Sin descripción'}
-> ⍰ *Duración:* ${duration}s
-> ♫ *Música:* ${music_info?.title || 'Original'}
-> ×͜× *Creador:* ${music_info?.author || '---'}
+> ღ *Autor:* ${data.autor?.apodo || data.author?.nickname || 'Anónimo'}
+> ✎ *Título:* ${data.title || 'Sin descripción'}
+> ⍰ *Duración:* ${data.duración || data.duration || 0}s
+> ♫ *Música:* ${data.music_info?.title || 'Original'}
+> ×͜× *Creador:* ${data.music_info?.autor || '---'}
 \t\t\t*ム ESTADÍSTICAS:*
-> 𖤍 *Vistas:* ${formatter.format(play_count || 0)}
-> ♡ *Likes:* ${formatter.format(digg_count || 0)}
-> ♛ *Comments:* ${formatter.format(comment_count || 0)}
-> ★ *Shares:* ${formatter.format(share_count || 0)}`
+> 𖤍 *Vistas:* ${formatter.format(data.recuento_de_reproducciones || data.play_count || 0)}
+> ♡ *Likes:* ${formatter.format(data.recuento_de_digg || data.digg_count || 0)}
+> ♛ *Comments:* ${formatter.format(data.Recuento_de_comentarios || data.comment_count || 0)}
+> ★ *Shares:* ${formatter.format(data.Recuento_de_acciones || data.share_count || 0)}`
 
             await conn.sendMessage(m.chat, { 
-                video: { url: play }, // Usamos 'play' directamente
+                video: { url: videoUrl },
                 caption: caption,
-                fileName: `tiktok_hd.mp4`,
+                fileName: `tiktok_voker.mp4`,
                 mimetype: 'video/mp4'
             }, { quoted: m })
 
