a8cfefe7c3db55c9bac28176d87c9e0b50f46b71
deylin-16 <deylin1616@gmail.com>
2026-03-12 16:02:55 -0600

    Actualizar antilink.js

--- plugins/group/antilink.js
+++ plugins/group/antilink.js
@@ -1,5 +1,5 @@
 import { jidNormalizedUser } from '@whiskeysockets/baileys';
-import { getRealJid } from '../../identifier.js';
+import { getRealJid } from '../../lib/identifier.js';
 
 export default function(conn) {
     conn.ev.on('messages.upsert', async (chatUpdate) => {
