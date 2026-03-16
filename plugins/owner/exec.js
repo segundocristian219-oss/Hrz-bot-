1864cbe6d3d7bd0e92cc4503784edb0af8a2829a
Eliac <deylin1616@gmail.com>
2026-03-16 13:49:14 -0600

    Actualizar exec.js

--- plugins/owner/exec.js
+++ plugins/owner/exec.js
@@ -1,25 +1,32 @@
-import cp, {exec as _exec} from 'child_process';
-import {promisify} from 'util';
-const exec = promisify(_exec).bind(cp);
-const handler = async (m, {conn, isOwner, command, text, usedPrefix, args, isROwner}) => {
-  if (!isROwner) return;
-  if (global.conn.user.jid != conn.user.jid) return;
-  m.reply(`✎ *Ejecutando espera...   ↻*`);
-  let o;
-  try {
-    o = await exec(command.trimStart() + ' ' + text.trimEnd());
-  } catch (e) {
-    o = e;
-  } finally {
-    const {stdout, stderr} = o;
-    if (stdout.trim()) m.reply(stdout);
-    if (stderr.trim()) m.reply(stderr);
-  }
-};
-handler.help = ['$']
-handler.tags = ['owner']
-handler.customPrefix = ['$']
-handler.command = new RegExp
-handler.rowner = true
+import cp, { exec as _exec } from 'child_process';
+import { promisify } from 'util';
 
-export default handler
+const exec = promisify(_exec).bind(cp);
+
+const shellCommand = {
+    name: 'shell',
+    alias: ['$'],
+    category: 'owner',
+    run: async (m, { conn, text, command, isROwner }) => {
+        if (!isROwner) return;
+        if (global.conn.user.jid != conn.user.jid) return;
+
+        if (!text) return;
+
+        await m.react('🕓');
+        
+        let o;
+        try {
+            o = await exec(text.trim());
+        } catch (e) {
+            o = e;
+        } finally {
+            const { stdout, stderr } = o;
+            if (stdout && stdout.trim()) await m.reply(stdout.trim());
+            if (stderr && stderr.trim()) await m.reply(stderr.trim());
+            await m.react('✅');
+        }
+    }
+};
+
+export default shellCommand;
