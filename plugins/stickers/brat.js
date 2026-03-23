93975d7a2afec60a00af22dff2fa90509838b389
Eliac <deylin1616@gmail.com>
2026-03-23 16:08:16 -0600

    Actualizar brat.js

--- plugins/stickers/brat.js
+++ plugins/stickers/brat.js
@@ -87,8 +87,7 @@ const bratCommand = {
                     forwardingScore: 1, 
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363406846602793@newsletter',
-                        serverMessageId: 100,
+                        newsletterJid: ch,
                         newsletterName: name()
                     }
                 }
