c86c0fb0d2b5d3d7675f4b959047dd2683f5d8c3
Eliac <deylin1616@gmail.com>
2026-03-25 13:16:06 -0600

    Actualizar prueba.js

--- plugins/prueba.js
+++ plugins/prueba.js
@@ -3,24 +3,32 @@ const h = {
     alias: ['rc'],
     category: 'main',
     run: async (m, { conn, text, usedPrefix, command }) => {
-        if (!text) return m.reply(`*Formato incorrecto*\nUso: ${usedPrefix + command} [link-mensaje] [emoji]\n\nEjemplo:\n${usedPrefix + command} https://whatsapp.com/channel/0029Vb8JHz6Fcow9KCuBVp1K/178 ✅`);
+        if (!text) return m.reply(`*Formato incorrecto*\nUso: ${usedPrefix + command} [link-mensaje] [emoji]`);
 
         const [link, emoji] = text.split(' ');
         if (!link || !emoji) return m.reply('❌ Falta el enlace o el emoji.');
 
         try {
-            const parts = link.split('/');
+            const parts = link.trim().split('/');
             const serverId = parts[parts.length - 1];
             const inviteCode = parts[parts.length - 2];
 
             const metadata = await conn.newsletterMetadata("invite", inviteCode);
             const jid = metadata.id;
 
-            await conn.newsletterReactions(jid, serverId, emoji);
-            m.reply(`✅ Reacción enviada a:\n*ID:* ${serverId}\n*Canal:* ${metadata.name}`);
+            await conn.query({
+                tag: 'message',
+                attrs: { to: jid, type: 'reaction', server_id: serverId },
+                content: [{
+                    tag: 'reaction',
+                    attrs: { code: emoji }
+                }]
+            });
+
+            m.reply(`✅ Reacción enviada con éxito.`);
 
         } catch (e) {
-            m.reply(`❌ Error: Verifica que el bot siga el canal o que el link sea válido.\n\n${e.message}`);
+            m.reply(`❌ Error:\n${e.message}`);
         }
     }
 };
