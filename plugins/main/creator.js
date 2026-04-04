529a62efcdf00ddc99093cee56b05d0de2fa80c3
Eliac <deylin1616@gmail.com>
2026-04-04 15:22:53 -0600

    Actualizar creator.js

--- plugins/main/creator.js
+++ plugins/main/creator.js
@@ -40,45 +40,40 @@ const handler = {
 
         } else {
             const num1 = '584228028583'
-            const num2 = '5493772455367'
-            const name1 = 'ϛαριϛυγ ♛'
-            const name2 = 'AlexDev09 ♛'
+const num2 = '5493772455367'
+const name1 = 'ϛαριϛυγ ♛'
+const name2 = 'AlexDev09 ♛'
 
-            const vcard1 = 'BEGIN:VCARD\n' +
-                          'VERSION:3.0\n' +
-                          `FN:${name1}\n` +
-                          'ORG:KiritoBot-MD Developer;\n' +
-                          'TITLE:Chief Executive Officer;\n' +
-                          `TEL;type=CELL;type=VOICE;waid=${num1}:${num1}\n` +
-                          'X-WA-BIZ-DESCRIPTION:programador, owner y desarrollador principal del bot.\n' +
-                          `X-WA-BIZ-NAME:${name1}\n` +
-                          'END:VCARD'
+const vcard1 = 'BEGIN:VCARD\n' +
+              'VERSION:3.0\n' +
+              `FN:${name1}\n` +
+              'ORG:KiritoBot-MD Team;\n' +
+              'TITLE:Desarrollador Principal & Founder;\n' + 
+              `TEL;type=CELL;type=VOICE;waid=${num1}:${num1}\n` +
+              'EMAIL;type=INTERNET:soporte@kiritobot.com\n' +
+              'URL:https://github.com/SebastianHerrera\n' + 
+              'X-WA-BIZ-DESCRIPTION:Programación de sistemas y arquitectura de bots.\n' +
+              `X-WA-BIZ-NAME:${name1}\n` +
+              'END:VCARD'
 
-            const vcard2 = 'BEGIN:VCARD\n' +
-                          'VERSION:3.0\n' +
-                          `FN:${name2}\n` +
-                          'ORG:KiritoBot-MD Developer;\n' +
-                          'TITLE:Chief Executive Officer;\n' +
-                          `TEL;type=CELL;type=VOICE;waid=${num2}:${num2}\n` +
-                          'X-WA-BIZ-DESCRIPTION:manejo de bots y scrappers.\n' +
-                          `X-WA-BIZ-NAME:${name2}\n` +
-                          'END:VCARD'
+const vcard2 = 'BEGIN:VCARD\n' +
+              'VERSION:3.0\n' +
+              `FN:${name2}\n` +
+              'ORG:KiritoBot-MD Team;\n' +
+              'TITLE:Administrador de Sistemas & Scrapers;\n' + 
+              `TEL;type=CELL;type=VOICE;waid=${num2}:${num2}\n` +
+              'EMAIL;type=INTERNET:dev@alex09.com\n' + 
+              'URL:https://dix.lat\n' + 
+              'X-WA-BIZ-DESCRIPTION:Especialista en extracción de datos y APIs.\n' +
+              `X-WA-BIZ-NAME:${name2}\n` +
+              'END:VCARD'
 
-    await conn.sendMessage(m.chat, {
+await conn.sendMessage(m.chat, {
     contacts: {
         displayName: 'Equipo de Desarrollo KiritoBot',
         contacts: [{ vcard: vcard1 }, { vcard: vcard2 }]
     },
     contextInfo: {
-        externalAdReply: {
-            title: 'KiritoBot-MD™ OFFICIAL CONTACTS',
-            body: 'KiritoBot-MD Development Team',
-            thumbnailUrl: img(),
-            sourceUrl: `https://wa.me/${num1}`,
-            mediaType: 1,
-            showAdAttribution: false,
-            renderLargerThumbnail: true
-        },
         businessOwnerJid: num1 + '@s.whatsapp.net'
     }
 }, { quoted: m })
