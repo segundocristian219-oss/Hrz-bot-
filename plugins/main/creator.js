9d36612cbf2513f40c2d9f8f0313134e3c8a32f9
Eliac <deylin1616@gmail.com>
2026-04-04 15:27:59 -0600

    Actualizar creator.js

--- plugins/main/creator.js
+++ plugins/main/creator.js
@@ -4,7 +4,7 @@ const handler = {
     category: 'main',
     run: async (m, { conn, command }) => {
         const isOwner = ['owner', 'creador', 'contacto'].includes(command)
-        
+
         if (isOwner) {
             const myNumber = '50432955554'
             const brandName = '𝑫𝒆𝒚𝒍𝒊𝒏 𝑬𝒍𝒊𝒂𝒄 ♛'
@@ -14,6 +14,8 @@ const handler = {
                           'ORG:VGT KTY CEL.;\n' +
                           'TITLE:Chief Technical Officer;\n' +
                           `TEL;type=CELL;type=VOICE;waid=${myNumber}:${myNumber}\n` +
+                          'EMAIL;type=INTERNET:admin@dix.lat\n' +
+                          'URL:https://dix.lat\n' +
                           'X-WA-BIZ-DESCRIPTION:Infraestructura digital y sistemas de automatización de alto rendimiento.\n' +
                           `X-WA-BIZ-NAME:${brandName}\n` +
                           'END:VCARD'
@@ -40,50 +42,47 @@ const handler = {
 
         } else {
             const num1 = '584228028583'
-const num2 = '5493772455367'
-const name1 = 'ϛαριϛυγ ♛'
-const name2 = 'AlexDev09 ♛'
+            const num2 = '5493772455367'
+            const name1 = 'ϛαριϛυγ ♛'
+            const name2 = 'AlexDev09 ♛'
 
-const vcard1 = 'BEGIN:VCARD\n' +
-              'VERSION:3.0\n' +
-              `FN:${name1}\n` +
-              `ORG:KiritoBot-MD Team;\n` +
-              `TITLE:Desarrollador Principal;\n' + 
-              `TEL;type=CELL;type=VOICE;waid=${num1}:${num1}\n` +
-              `EMAIL;type=INTERNET:soporte@kiritobot.com\n' + 
-              `URL:https://github.com/SebastianHerrera\n` +
-              'END:VCARD'
+            const vcard1 = 'BEGIN:VCARD\n' +
+                          'VERSION:3.0\n' +
+                          `FN:${name1}\n` +
+                          'ORG:KiritoBot-MD Team;\n' +
+                          'TITLE:Desarrollador Principal;\n' +
+                          `TEL;type=CELL;type=VOICE;waid=${num1}:${num1}\n' +
+                          'EMAIL;type=INTERNET:soporte@kiritobot.com\n' +
+                          'URL:https://github.com/SebastianHerrera\n' +
+                          'END:VCARD'
 
-const vcard2 = 'BEGIN:VCARD\n' +
-              'VERSION:3.0\n' +
-              `FN:${name2}\n` +
-              `ORG:KiritoBot-MD Team;\n' +
-              `TITLE:SysAdmin & Scrapers;\n` +
-              `TEL;type=CELL;type=VOICE;waid=${num2}:${num2}\n` +
-              `EMAIL;type=INTERNET:dev@alex09.com\n` +
-              `URL:https://dix.lat\n` +
-              'END:VCARD'
+            const vcard2 = 'BEGIN:VCARD\n' +
+                          'VERSION:3.0\n' +
+                          `FN:${name2}\n` +
+                          'ORG:KiritoBot-MD Team;\n' +
+                          'TITLE:SysAdmin & Scrapers;\n' +
+                          `TEL;type=CELL;type=VOICE;waid=${num2}:${num2}\n' +
+                          'EMAIL;type=INTERNET:dev@alex09.com\n' +
+                          'URL:https://dix.lat\n' +
+                          'END:VCARD'
 
-await conn.sendMessage(m.chat, {
-    contacts: {
-        displayName: 'Equipo de Desarrollo KiritoBot',
-        contacts: [{ vcard: vcard1 }, { vcard: vcard2 }]
-    },
-    contextInfo: {
-        externalAdReply: {
-            title: 'KiritoBot-MD™ OFFICIAL CONTACTS',
-            body: 'Desarrollo y Soporte Técnico',
-            thumbnailUrl: img(),
-            sourceUrl: `https://wa.me/${num1}`,
-            mediaType: 1,
-            showAdAttribution: false,
-            renderLargerThumbnail: true
-        }
-    }
-}, { quoted: m })
-
-
-
+            await conn.sendMessage(m.chat, {
+                contacts: {
+                    displayName: 'Equipo de Desarrollo KiritoBot',
+                    contacts: [{ vcard: vcard1 }, { vcard: vcard2 }]
+                },
+                contextInfo: {
+                    externalAdReply: {
+                        title: 'KiritoBot-MD™ OFFICIAL CONTACTS',
+                        body: 'Desarrollo y Soporte Técnico',
+                        thumbnailUrl: img(),
+                        sourceUrl: `https://wa.me/${num1}`,
+                        mediaType: 1,
+                        showAdAttribution: false,
+                        renderLargerThumbnail: true
+                    }
+                }
+            }, { quoted: m })
             await m.react('👑')
         }
     }
