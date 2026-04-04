c2189be81fe477e94d50062bd120dbd6749cadf7
CAPICUY <capicuy.sh@gmail.com>
2026-04-04 01:49:05 -0400

    Update devs.js

--- plugins/main/devs.js
+++ plugins/main/devs.js
@@ -1,20 +1,20 @@
 const devsCommand = {
     name: 'devs',
-    alias: ['developer', 'creadora', 'owner', 'soporte'],
+    alias: ['developer', 'desarrolladores', 'soporte'],
     category: 'main',
     run: async (m, { conn }) => {
         const myNumber = '5842228583'
         const myJid = myNumber + '@s.whatsapp.net'
-        const brandName = '𝑺𝒐𝒑𝒉𝒊𝒂 𝑳𝒖𝒈𝒐 ♛' 
+        const brandName = 'ϛαριϛυγ ♛' 
 
         const vcard = 'BEGIN:VCARD\n' +
                       'VERSION:3.0\n' +
                       `FN:${brandName}\n` + 
                       `N:;Lugo;Sophia;;\n` +
-                      'ORG:YukiBot-MD Developer;\n' + 
+                      'ORG:KiritoBot-MD Developer;\n' + 
                       'TITLE:Chief Executive Officer;\n' + 
                       `TEL;type=CELL;type=VOICE;waid=${myNumber}:${myNumber}\n` +
-                      'X-WA-BIZ-DESCRIPTION:Desarrollo de sistemas de automatización y gestión de bots.\n' +
+                      'X-WA-BIZ-DESCRIPTION:Desarrollo de software y automatización manejo de bots y programador.\n' +
                       `X-WA-BIZ-NAME:${brandName}\n` +
                       'END:VCARD'
 
@@ -25,8 +25,8 @@ const devsCommand = {
             },
             contextInfo: {
                 externalAdReply: {
-                    title: 'SOPHIA™ OFFICIAL CONTACT',
-                    body: 'YukiBot-MD Development Team',
+                    title: 'Kirito™ OFFICIAL CONTACT',
+                    body: 'KiritoBot-MD Development Team',
                     thumbnailUrl: img(), // Asegúrate de que img() esté definido en tu global
                     sourceUrl: `https://wa.me/${myNumber}`,
                     mediaType: 1,
