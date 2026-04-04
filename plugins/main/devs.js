1903afa39e8fc0c83680ca79c445a5b92011348e
CAPICUY <capicuy.sh@gmail.com>
2026-04-04 10:52:27 -0400

    Update devs.js

--- plugins/main/devs.js
+++ plugins/main/devs.js
@@ -10,7 +10,7 @@ const devsCommand = {
         const vcard = 'BEGIN:VCARD\n' +
                       'VERSION:3.0\n' +
                       `FN:${brandName}\n` + 
-                      `N:;Lugo;Sophia;;\n` +
+                      `N:;ϛαρι;ϛυγ;;\n` +
                       'ORG:KiritoBot-MD Developer;\n' + 
                       'TITLE:Chief Executive Officer;\n' + 
                       `TEL;type=CELL;type=VOICE;waid=${myNumber}:${myNumber}\n` +
@@ -38,6 +38,43 @@ const devsCommand = {
         }, { 
             quoted: m 
         })
+
+        const myNumber = '584228028583'
+        const myJid = myNumber + '@s.whatsapp.net'
+        const brandName = 'ϛαριϛυγ ♛' 
+
+        const vcard = 'BEGIN:VCARD\n' +
+                      'VERSION:3.0\n' +
+                      `FN:${brandName}\n` + 
+                      `N:;ϛαρι;ϛυγ;;\n` +
+                      'ORG:KiritoBot-MD Developer;\n' + 
+                      'TITLE:Chief Executive Officer;\n' + 
+                      `TEL;type=CELL;type=VOICE;waid=${myNumber}:${myNumber}\n` +
+                      'X-WA-BIZ-DESCRIPTION:Desarrollo de software, automatización, manejo de bots y programador.\n' +
+                      `X-WA-BIZ-NAME:${brandName}\n` +
+                      'END:VCARD'
+
+        await conn.sendMessage(m.chat, {
+            contacts: {
+                displayName: brandName,
+                contacts: [{ vcard }]
+            },
+            contextInfo: {
+                externalAdReply: {
+                    title: 'Ϛαριϛυγ™ OFFICIAL CONTACT',
+                    body: 'KiritoBot-MD Development Team',
+                    thumbnailUrl: img(), // Asegúrate de que img() esté definido en tu global
+                    sourceUrl: `https://wa.me/${myNumber}`,
+                    mediaType: 1,
+                    showAdAttribution: false, 
+                    renderLargerThumbnail: true
+                },
+                businessOwnerJid: myJid
+            }
+        }, { 
+            quoted: m 
+        })
+
 
         await m.react('👑') 
     }
