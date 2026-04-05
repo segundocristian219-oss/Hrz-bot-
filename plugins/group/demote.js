94fa71c0a4575b14600739b3857df1d9d3634829
CAPICUY <capicuy.sh@gmail.com>
2026-04-04 22:55:23 -0400

    Update demote.js

--- plugins/group/demote.js
+++ plugins/group/demote.js
@@ -3,12 +3,31 @@ const demoteCommand = {
     alias: ['quitaradmin', 'unadmin'],
     category: 'owner',
     group: true,
-    botAdmin: true,
-    admin: true,
     run: async (m, { conn, usedPrefix, command, isAdmin, isBotAdmin }) => {
         try {
+            const isOwner = global.owner.map(v => v[0] + '@s.whatsapp.net').includes(m.sender);
+            if (!isOwner) {
+                global.dfail('owner', m, conn);
+                return;
+            }
+
+            if (!isBotAdmin) {
+                global.dfail('botAdmin', m, conn);
+                return;
+            }
 
             let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : m.sender;
+
+            const groupMetadata = await conn.groupMetadata(m.chat);
+            const ownerGroup = groupMetadata.owner || m.chat.split`-`[0] + '@s.whatsapp.net';
+
+            if (who === ownerGroup) {
+                return conn.reply(m.chat, `*─── [ ❌ ADVERTENCIA ] ───*\n\n_No es posible quitarle el administrador a @${who.split('@')[0]} porque es el *Creador del Grupo*._\n\n_Jerarquía de WhatsApp respetada._`, m, { mentions: [who] });
+            }
+
+            if (who === m.sender && !isAdmin) {
+                return conn.reply(m.chat, `*─── [ ✅ INFO ] ───*\n\n_No eres administrador, por lo que no es necesario el comando._`, m);
+            }
 
             let d = new Date();
             let time = d.toLocaleTimeString('es-HN', { hour: 'numeric', minute: 'numeric', hour12: true });
@@ -17,7 +36,7 @@ const demoteCommand = {
             try {
                 await conn.groupParticipantsUpdate(m.chat, [who], 'demote');
 
-                let txt = `*─── [ ⍰ DEMOTE ] ───*\n\n`;
+                let txt = `*─── [ 🛡️ DEMOTE ] ───*\n\n`;
                 txt += `*♛ Usuario:* @${who.split('@')[0]}\n`;
                 txt += `*✰ Estado:* Administrador removido\n`;
                 txt += `*➠ Fecha:* ${date} | ${time}\n\n`;
@@ -37,3 +56,4 @@ const demoteCommand = {
 };
 
 export default demoteCommand;
+                
