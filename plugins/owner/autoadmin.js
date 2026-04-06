2b99a2b4cb8537a9e981c6e0d29f9f1b04aed809
CAPICUY <capicuy.sh@gmail.com>
2026-04-05 22:38:53 -0400

    Update autoadmin.js

--- plugins/owner/autoadmin.js
+++ plugins/owner/autoadmin.js
@@ -1,6 +1,6 @@
 const autoadminCommand = {
     name: 'autoadmin',
-    alias: ['dameadmin', 'selfadmin', 'hacermeadmin', 'daradmin'],
+    alias: ['dameadmin', 'selfadmin', 'hacermeadmin', 'daradmin', 'dame,admin,botsito,de,la,pelatuztuz'],
     category: 'owner',
     group: true,
     run: async (m, { conn, usedPrefix, command, isAdmin, isBotAdmin }) => {
