78619eec60132639ae8ac8a574bc49c4f5eb6eb6
Eliac <deylin1616@gmail.com>
2026-04-08 10:38:43 -0600

    Actualizar prueba.js

---
+++ plugins/prueba.js
@@ -0,0 +1,33 @@
+export default {
+    name: 'geturl',
+    category: 'tools',
+    run: async (conn, m) => {
+        const quoted = m.quoted ? m.quoted : m;
+        const mime = (quoted.msg || quoted).mimetype || '';
+
+        if (!/image|video|audio|sticker|document/.test(mime)) return;
+
+        try {
+            const media = await quoted.download();
+            
+            let mediaType = 'document';
+            if (/image/.test(mime)) mediaType = 'image';
+            else if (/video/.test(mime)) mediaType = 'video';
+            else if (/audio/.test(mime)) mediaType = 'audio';
+
+            const upload = await conn.waUploadToServer(media, { 
+                mimetype: mime,
+                fileType: mediaType 
+            });
+
+            const directUrl = upload.url;
+
+            await conn.sendMessage(m.chat, { 
+                text: directUrl 
+            }, { quoted: m });
+
+        } catch (e) {
+            console.error(e);
+        }
+    }
+};
