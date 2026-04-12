d650dae9d7dd14f30a9f1ba9754eb196874f384d
Eliac <deylin1616@gmail.com>
2026-04-11 20:28:38 -0600

    Actualizar serbot.js

--- plugins/main/serbot.js
+++ plugins/main/serbot.js
@@ -5,30 +5,31 @@ const serbot = {
     name: 'serbot',
     alias: ['code', 'subbot', 'jadibot'],
     category: 'main',
-    run: async (m, { conn, args, usedPrefix, command }) => {
+    run: async (m, { conn, args, usedPrefix, command, isPremiumBot }) => {
         const groupOfficial = '120363407713231046@g.us';
         const groupLink = 'https://chat.whatsapp.com/CkizHMducS3H0wYsmuCHU6';
 
-        if (m.chat !== groupOfficial) {
-            const onlyInGroup = `*COMANDO RESTRINGIDO*\n\n` +
-                                 `ESTE COMANDO SOLO PUEDE SER EJECUTADO DENTRO DEL GRUPO OFICIAL.\n\n` +
-                                 `*UNIRSE AQUÍ:*\n` +
-                                 `${groupLink}`;
+        if (isPremiumBot) {
+            const premiumMessage = `*SISTEMA PREMIUM ACTIVO*\n\n` +
+                                   `ESTA INSTANCIA DEL BOT ES *PREMIUM* Y NO PERMITE LA CREACIÓN DE SUB-BOTS DIRECTAMENTE.\n\n` +
+                                   `SI DESEAS CONVERTIRTE EN SUB-BOT GRATUITO, ÚNETE A NUESTRO GRUPO OFICIAL:\n` +
+                                   `${groupLink}`;
 
             return await conn.sendMessage(m.chat, {
-                text: onlyInGroup,
+                text: premiumMessage,
                 contextInfo: {
                     externalAdReply: {
-                        title: global.name(),
-                        body: 'OPERACIÓN DENEGADA',
+                        title: '👑 PREMIUM SYSTEM',
+                        body: 'SUB-BOTS DESACTIVADOS',
                         mediaType: 1,
                         thumbnailUrl: global.img(),
-                        sourceUrl: 'https://dix.lat',
+                        sourceUrl: groupLink,
                         renderLargerThumbnail: false
                     }
                 }
             }, { quoted: m });
         }
+
 
         const senderId = jidNormalizedUser(m.sender);
         let targetId = args[0] ? args[0].replace(/[^0-9]/g, '') : senderId.split('@')[0];
