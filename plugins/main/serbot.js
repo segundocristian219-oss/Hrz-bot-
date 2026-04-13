cf4fcbb923c32393ad230971fe1b071ff6ad50bf
Eliac <deylin1616@gmail.com>
2026-04-12 20:25:50 -0600

    Actualizar serbot.js

--- plugins/main/serbot.js
+++ plugins/main/serbot.js
@@ -9,31 +9,22 @@ const serbot = {
         const groupOfficial = '120363407713231046@g.us';
         const groupLink = 'https://chat.whatsapp.com/CkizHMducS3H0wYsmuCHU6';
 
-
         const senderId = jidNormalizedUser(m.sender);
-        let targetId = args[0] ? args[0].replace(/[^0-9]/g, '') : senderId.split('@')[0];
+        let targetNum = args[0] ? args[0].replace(/[^0-9]/g, '') : senderId.split('@')[0];
+        let targetJid = targetNum + '@s.whatsapp.net';
 
-        const instructions = ` *SISTEMA VINCULACIÓN* \n` +
-                             `\n` +
-                             ` 1. DISPOSITIVOS VINCULADOS\n` +
-                             ` 2. VINCULAR CON NÚMERO\n` +
-                             ` 3. INGRESAR EL TOKEN ENVIADO\n` +
-                             `\n` +
-                             ` *SOLICITUD PARA:*\n` +
-                             ` ${targetId}\n` +
-                             `\n` +
-                             `> *NOTA:*\n` +
-                             `> Esté código vence en 8 segundos y solo es válido para ${targetId}`;
+        const instructions = `『 *${name()}* 』\n\n✦ 1. DISPOSITIVOS VINCULADOS\n✦ 2. VINCULAR CON NÚMERO\n✦ 3. INGRESAR EL CÓDIGO ENVIADO\n\n*SOLICITUD PARA:*\n@${targetNum}\n\n† *NOTA:* este código vence en 8 segundos y solo es válido para @${targetNum}`;
 
         await conn.sendMessage(m.chat, {
             image: { url: global.img() },
             caption: instructions,
-            contextinfo: {
-              ...channelInfo
-          }
+            mentions: [targetJid],
+            contextInfo: {
+                ...channelInfo
+            }
         }, { quoted: m });
 
-        await startSubBot(m, conn, targetId);
+        await startSubBot(m, conn, targetNum);
     }
 };
 
