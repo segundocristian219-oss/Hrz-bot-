25d4e74d30f79b0807a1473ce521cd1d8787da51
Eliac <deylin1616@gmail.com>
2026-04-15 15:07:39 -0600

    Actualizar info-bot.js

--- plugins/main/info-bot.js
+++ plugins/main/info-bot.js
@@ -2,12 +2,15 @@ const info = {
     name: 'infobot',
     alias: ['info'],
     category: 'owner',
-    run: async (m, { conn, text, isROwner, command }) => {
+    run: async (m, { conn, text, command }) => {
      let txt = 'hola'
      await conn.sendMessage(m.chat, {
-      caption: txt
+      text: txt
     }, { quoted: m });
-};
+   }
+  };
+
+
 
 
 export default info;
