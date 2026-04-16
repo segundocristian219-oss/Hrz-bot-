b9dc66f15e2fae5c13a4f5bae20600f7ab1e9823
Eliac <deylin1616@gmail.com>
2026-04-16 12:17:30 -0600

    Actualizar youtube.js

--- plugins/search/youtube.js
+++ plugins/search/youtube.js
@@ -11,32 +11,36 @@ const ytSearchCommand = {
 
         try {
             const search = await yts(text);
-            const vid = search.videos[0];
+            
+            const vids = search.videos.slice(0, 3);
 
-            if (!vid) {
+            if (vids.length === 0) {
                 m.react('❌');
                 return m.reply(`❯❯ 𝗘𝗥𝗥𝗢𝗥: No se detectaron resultados.`);
             }
 
-            const mensaje = `❯❯ 𝗬𝗢𝗨𝗧𝗨𝗕𝗘 𝗦𝗘𝗔𝗥𝗖𝗛\n\n` +
-                            `❖ 𝗧𝗜𝗧𝗨𝗟𝗢: ${vid.title}\n` +
-                            `❖ 𝗜𝗗: ${vid.videoId}\n` +
-                            `❖ 𝗗𝗨𝗥𝗔𝗖𝗜𝗢𝗡: ${vid.timestamp}\n` +
-                            `❖ 𝗩𝗜𝗦𝗧𝗔𝗦: ${vid.views.toLocaleString()}\n` +
-                            `❖ 𝗣𝗨𝗕𝗟𝗜𝗖𝗔𝗗𝗢: ${vid.ago}\n` +
-                            `❖ 𝗔𝗨𝗧𝗢𝗥: ${vid.author.name}\n` +
-                            `❖ 𝗘𝗡𝗟𝗔𝗖𝗘: ${vid.url}`;
+            let mensaje = `❯❯ 𝗬𝗢𝗨𝗧𝗨𝗕𝗘 𝗦𝗘𝗔𝗥𝗖𝗛 𝗥𝗘𝗦𝗨𝗟𝗧𝗦\n\n`;
 
+            vids.forEach((vid, index) => {
+                mensaje += `*# RESULTADO ${index + 1}*\n` +
+                           `❖ 𝗧𝗜𝗧𝗨𝗟𝗢: ${vid.title}\n` +
+                           `❖ 𝗗𝗨𝗥𝗔𝗖𝗜𝗢𝗡: ${vid.timestamp}\n` +
+                           `❖ 𝗩𝗜𝗦𝗧𝗔𝗦: ${vid.views.toLocaleString()}\n` +
+                           `❖ 𝗔𝗨𝗧𝗢𝗥: ${vid.author.name}\n` +
+                           `❖ 𝗘𝗡𝗟𝗔𝗖𝗘: ${vid.url}\n\n`;
+            });
+
+            
             await conn.sendMessage(m.chat, { 
-                text: mensaje,
+                text: mensaje.trim(),
                 contextInfo: {
                     externalAdReply: {
-                        title: vid.title,
-                        body: `Canal: ${vid.author.name}`,
+                        title: `Resultados para: ${text}`,
+                        body: `Voker Systems Division`,
                         mediaType: 1,
-                        thumbnailUrl: vid.thumbnail,
+                        thumbnailUrl: vids[0].thumbnail,
                         renderLargerThumbnail: true,
-                        sourceUrl: vid.url
+                        sourceUrl: vids[0].url
                     }
                 }
             }, { quoted: m });
@@ -46,6 +50,7 @@ const ytSearchCommand = {
         } catch (e) {
             console.error(e);
             m.react('❌');
+            m.reply('❯❯ 𝗘𝗥𝗥𝗢𝗥: Ocurrió un problema con la búsqueda.');
         }
     }
 };
