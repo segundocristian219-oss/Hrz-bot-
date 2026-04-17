d608623243cf5e47b2327a98089b5b633a6a884f
CAPICUY <capicuy.sh@gmail.com>
2026-04-17 00:59:53 -0400

    Update primary.js

--- plugins/group/primary.js
+++ plugins/group/primary.js
@@ -1,35 +1,6 @@
-const primaryGroups = global.primaryGroups || (global.primaryGroups = new Map())
-
-const primaryCommand = {
+export default {
     name: 'setprimary',
-    alias: ['delprimary'],
-    category: 'group',
-    group: true,
-
-    run: async (m, { conn, isROwner }) => {
-        try {
-            const chat = m.chat
-            const botId = conn.user.id
-
-            /* if (!isROwner) return */
-
-            const cmd = m.command
-
-            if (cmd === 'setprimary') {
-                primaryGroups.set(chat, botId)
-                return conn.reply(chat, '*✰ Primary activado*', m)
-            }
-
-            if (cmd === 'delprimary') {
-                primaryGroups.delete(chat)
-                return conn.reply(chat, '*✰ Primary desactivado*', m)
-            }
-
-        } catch (e) {
-            console.error('PRIMARY ERROR:', e)
-            return conn.reply(m.chat, 'Error', m)
-        }
+    run: async (m, { conn }) => {
+        await conn.sendMessage(m.chat, { text: 'funciona' }, { quoted: m })
     }
 }
-
-export default primaryCommand
