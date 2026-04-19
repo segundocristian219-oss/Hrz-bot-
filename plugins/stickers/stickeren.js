1036474f4aae22a5ea02e35018da693923cabe86
CAPICUY <capicuy.sh@gmail.com>
2026-04-19 18:17:55 -0400

    Update stickeren.js

--- plugins/stickers/stickeren.js
+++ plugins/stickers/stickeren.js
@@ -30,7 +30,7 @@ async function addExif(webpSticker, packname, author, categories = ["🤩"], ext
 
 const wmCommand = {
     name: 'wm',
-    alias: ['take', 'robar', 'stickerwm'],
+    alias: ['take', 'quitar'. 'stickerwm'],
     category: 'sticker',
     run: async (m, { conn, text }) => {
         try {
