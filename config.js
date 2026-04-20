5e8f51892a7edd83ffb40dd7050216d7dcddf409
Eliac <deylin1616@gmail.com>
2026-04-19 19:41:10 -0600

    Actualizar config.js

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
+
+
 import { watchFile, unwatchFile } from 'fs'
 import chalk from 'chalk'
 import { fileURLToPath } from 'url'
@@ -10,40 +12,51 @@ import path from 'path'
 
 global.owner = [['50432955554'], ['584228028583']]
 
+
+
 global.botNames = [
   '𝐊𝐈𝐑𝐈𝐓𝐎 ♛',       
-  '𝒌𝒂𝒛𝒖𝒕𝒐 𝒌𝒊𝒓𝒊𝒈𝒂ʏ ✰', 
+  '𝒌𝒂𝒛𝒖𝒕𝒐 𝒌𝒊𝒓𝒊𝒈𝒂𝒚 ✰', 
   '⧫ ᴋɪʀɪᴛᴏ - ᴋᴀᴢᴜᴛᴏ ᴋɪʀɪɢᴀʏ ⧫',   
-  '偏移 🄺🄸🅁🄸🅃🄾 偏移'       
+  '⌬ 🄺🄸🅁🄸🅃🄾 ⌬'       
 ]
 
-global.botImages = [ 
-  'https://api.dix.lat/media2/1773637281084.jpg',
-  'https://api.dix.lat/media2/1773637276760.jpg',
-  'https://api.dix.lat/media2/1773637265253.jpg',
-  'https://api.dix.lat/media2/1773637270663.jpg',
-  'https://api.dix.lat/media2/1773637244306.jpg'
-]
 
-global.botImages2 = [ 
-  'https://api.dix.lat/media2/1773638458604.jpg',
-  'https://api.dix.lat/media2/1773638453930.jpg',
-  'https://api.dix.lat/media2/1773638443955.jpg',
-  'https://api.dix.lat/media2/1773638448409.jpg',
-  'https://api.dix.lat/media2/1773638462741.jpg',
-  'https://api.dix.lat/media2/1773638468052.jpg'
-]
+global.botImages = [ 'https://api.dix.lat/media2/1773637281084.jpg',
+'https://api.dix.lat/media2/1773637276760.jpg',
+'https://api.dix.lat/media2/1773637265253.jpg',
+'https://api.dix.lat/media2/1773637270663.jpg',
+'https://api.dix.lat/media2/1773637244306.jpg']
+
+global.botImages2 = [ 'https://api.dix.lat/media2/1773638458604.jpg',
+'https://api.dix.lat/media2/1773638453930.jpg',
+'https://api.dix.lat/media2/1773638443955.jpg',
+'https://api.dix.lat/media2/1773638448409.jpg',
+'https://api.dix.lat/media2/1773638462741.jpg',
+'https://api.dix.lat/media2/1773638468052.jpg']
 
 const conf = {
-  utils: { cheerio, fs, fetch, axios, moment },
-  api: { url: 'https://api.dix.lat', key: 'VOKER_FREE_2026' },
-  sessions: { main: 'sessions', sub: 'sessions_sub_assistant' },
-  social: { channel: '120363406846602793@newsletter' }
+  utils: {
+    cheerio,
+    fs,
+    fetch,
+    axios,
+    moment
+  },
+  api: {
+    url: 'https://api.dix.lat',
+    key: 'VOKER_FREE_2026'
+  },
+  sessions: {
+    main: 'sessions',
+    sub: 'sessions_sub_assistant'
+  },
+  social: {
+    channel: '120363406846602793@newsletter'
+  }
 }
-
 var more = String.fromCharCode(8206)
 Object.assign(global, conf.utils)
-
 global.url_api = conf.api.url
 global.key = conf.api.key
 global.sessions = conf.sessions.main
@@ -51,28 +64,37 @@ global.jadi = conf.sessions.sub
 global.ch = conf.social.channel
 global.rmr = more.repeat(850)
 global.developer = '𝙳𝚎𝚢𝚕𝚒𝚗 𝙴𝚕𝚒𝚊𝚌'
-
 global.name = () => global.botNames[Math.floor(Math.random() * global.botNames.length)]
 global.img = () => global.botImages[Math.floor(Math.random() * global.botImages.length)]
 global.img2 = () => global.botImages2[Math.floor(Math.random() * global.botImages2.length)]
+global.v = JSON.parse(fs.readFileSync('./package.json', 'utf-8')).version
+
+
+
 
 global.key = "kirito-bot-oficial"
+
+
 
 global.channelInfo = {
     forwardingScore: 1,
     isForwarded: true,
     forwardedNewsletterMessageInfo: {
-        newsletterJid: global.ch, 
-        newsletterName: global.name()
+        newsletterJid: ch, 
+        newsletterName: name()
     }
-}
+};
+
 
 global.getBuffer = async (url, options = {}) => {
   try {
     const res = await axios({
       method: "get",
       url,
-      headers: { 'DNT': 1, 'User-Agent': 'GoogleBot' },
+      headers: {
+        'DNT': 1,
+        'User-Agent': 'GoogleBot'
+      },
       ...options,
       responseType: 'arraybuffer'
     })
@@ -81,6 +103,7 @@ global.getBuffer = async (url, options = {}) => {
     return null
   }
 }
+
 
 const d = new Date(new Date().getTime() + 3600000)
 global.fecha = d.toLocaleDateString('es', { day: 'numeric', month: 'numeric', year: 'numeric' })
@@ -95,10 +118,3 @@ const hour = new Intl.DateTimeFormat('es-HN', {
 global.saludo = hour >= 6 && hour < 12 ? 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌅' : 
                  hour >= 12 && hour < 19 ? 'Lɪɴᴅᴀ Tᴀʀᴅᴇ 🌆' : 
                  'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃';
-
-let file = fileURLToPath(import.meta.url)
-watchFile(file, () => {
-  unwatchFile(file)
-  console.log(chalk.redBright(`Update 'config.js'`))
-  import(`${file}?update=${Date.now()}`)
-})
