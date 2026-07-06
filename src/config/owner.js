15a375dc0ca46813d1d1e60003a9537a5c469f76
segundocristian219-oss <segundocristian219@gmail.com>
2026-07-06 10:34:40 -0600

    Actualizar owner.js

--- src/config/owner.js
+++ src/config/owner.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 
-global.owner = [['50432955554'], ['573508941325'], ['16394582266'], ['5216711089134'], ['584228028583']];
+global.owner = [['217158512549931'], ['573508941325'], ['16394582266'], ['5216711089134'], ['584228028583']];
 global.dev1 = '50432955554';
-global.developer = '𝙳𝚎𝚢𝚕𝚒𝚗 𝙴𝚕𝚒𝚊𝚌';
+global.developer = 'hernandez hrz';
 global.key = process.env.API_KEY;
 global.v = JSON.parse(fs.readFileSync('./package.json', 'utf-8')).version;
